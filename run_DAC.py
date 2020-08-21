34be0adfcf6f872078fb6087dddbe23e8d34190c
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-08-21 15:13:11 -0600

    modify plotting scripts

--- run_DAC.py
+++ run_DAC.py
@@ -8,6 +8,7 @@ import os
 import wisdem
 import os
 import matplotlib.pyplot as plt
+import glob
 from wisdem.glue_code.runWISDEM import run_wisdem
 from wisdem.aeroelasticse.Util import ReadFASTout
 from ROSCO_toolbox.utilities import FAST_IO, FAST_Plots
@@ -24,12 +25,13 @@ fname_analysis_options = os.path.join(run_dir,"analysis_options.yaml")
 wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
 # Load OpenFAST output
-fast_output_file = [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BAR_00', 'OpenFAST_BAR_00.outb'),
-                    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BAR_00_dac', 'BAR_00_dac_powercurve_0.outb')]
+fast_output_files = [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BAR_00', 'OpenFAST_BAR_00.outb')]
+fast_output_files += glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BAR_00_dac', '*.outb'))
 
-fast_out = fio.load_FAST_out(fast_output_file, tmin=50)
+fast_out = fio.load_FAST_out(fast_output_files, tmin=50)
 
 plot_cases = {}
 plot_cases['Baseline'] = ['Wind1VelX', 'BldPitch1', 'RotSpeed', 'GenPwr','BLFLAP1', 'RootMyb1']
 
 fpl.plot_fast_out(plot_cases, fast_out, showplot='True')
+plt.tight_layout()
